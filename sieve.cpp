cfe110442efdb3f03da83a2dce421b3ddfa6d8ae
satyamjiyadav <satyamjiyadav12345@gmail.com>
2024-10-03 21:01:46 +0530

    primeNumber

---
+++ sieve.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void solve(){
+	
+// }
+
+// O(sqrt(r) * size) + O(size)
+int main(){
+	int r,l;
+	cin>>r>>l;
+
+	int size = r-l+1;
+
+	vector<bool>prime(size,true);
+
+	for(int i=2; i<=sqrt(r); i++){
+		for(int ind = 0; ind<size; ind++){
+			if((l+ind)%i == 0){
+				prime[ind] = false;
+			}
+		}
+	}
+
+	for(int ind = 0; ind<size; ind++){
+		if(prime[ind] == true){
+			cout<<ind+l<<" ";
+		}
+	}
+}
